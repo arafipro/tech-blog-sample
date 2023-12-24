c95863c5c4caef04ee3e9d6ba431d3885d14c1f9
Hiroki Chiyoda <contact@arafipro.com>
2023-12-24 17:33:23 +0900

    リファクタリング

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -1,8 +1,6 @@
-import {connect,disconnect} from "@/utils/db";
-import { PrismaClient } from "@prisma/client";
+import { connect, disconnect } from "@/utils/db";
+import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
 
 export const GET = async (req: Request, res: NextResponse) => {
   try {
