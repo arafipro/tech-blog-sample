7444f29b29d54cd07dc559e18c087e79a9e080e3
Hiroki Chiyoda <contact@arafipro.com>
2023-12-31 00:09:34 +0900

    getAllPosts関数の作成
    全ての投稿を取得する関数を作成

--- lib/blogApi.ts
+++ lib/blogApi.ts
@@ -1,5 +1,15 @@
 import { FormSchemaType } from "./validationSchema/FormSchema";
 
+export async function getAllPosts() {
+  const res = await fetch(`http://localhost:3000/api/blog/`, {
+    cache: "no-cache",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
 export async function postBlog(data: FormSchemaType) {
   const { title, content } = data;
   const res = await fetch(`http://localhost:3000/api/blog/`, {
