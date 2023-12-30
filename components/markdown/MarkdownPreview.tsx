5e80a32eed15a296b16111c100f2ef1813857ec7
Hiroki Chiyoda <contact@arafipro.com>
2023-12-30 15:59:34 +0900

    zenn-markdown-htmlを導入
    html-react-parserを導入

--- components/markdown/MarkdownPreview.tsx
+++ components/markdown/MarkdownPreview.tsx
@@ -1,8 +1,8 @@
 import { cn } from "@/lib/utils";
-import "highlight.js/styles/xcode.css";
-import Markdown from "react-markdown";
-import rehypeHighlight from "rehype-highlight";
-import remarkGfm from "remark-gfm";
+import parse from "html-react-parser";
+import "zenn-content-css";
+import markdownToHtml from "zenn-markdown-html";
+
 export default function MarkdownPreview({
   content,
   className,
@@ -10,50 +10,6 @@ export default function MarkdownPreview({
   content: string;
   className?: string;
 }) {
-  return (
-    <Markdown
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeHighlight]}
-      className={cn("space-y-6", className)}
-      components={{
-        h1: ({ node, ...props }) => {
-          return <h1 {...props} className="text-3xl font-bold" />;
-        },
-        h2: ({ node, ...props }) => {
-          return <h2 {...props} className="text-2xl font-bold" />;
-        },
-        h3: ({ node, ...props }) => {
-          return <h3 {...props} className="text-xl font-bold" />;
-        },
-        code: ({ node, className, children, ...props }) => {
-          const match = /language-(\w+)/.exec(className || "");
-          if (match?.length) {
-            return (
-              <div className=" bg-graident-dark text-gray-300 border rounded-md">
-                <div className="px-5 py-2 border-b flex items-center justify-between">
-                  <div className="flex items-center gap-2">
-                    <span>
-                      {
-                        // @ts-ignore
-                        node?.data?.meta
-                      }
-                    </span>
-                  </div>
-                </div>
-                <div className=" overflow-x-auto w-full">
-                  <div className="p-5">{children}</div>
-                </div>
-              </div>
-            );
-          } else {
-            return (
-              <code className="bg-zinc-700 rounded-md px-2">{children}</code>
-            );
-          }
-        },
-      }}
-    >
-      {content}
-    </Markdown>
-  );
+  const html = parse(markdownToHtml(content));
+  return <div className={cn("space-y-6 znc", className)}>{html}</div>;
 }
