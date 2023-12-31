241f4b512e271c6459585e2e42f89342e01b5922
Hiroki Chiyoda <contact@arafipro.com>
2023-12-31 15:24:32 +0900

    crypto-browserifyをインストール

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  resolve: {
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      // 他に必要なモジュールがあれば追加
+    },
+  },
+};
 
 module.exports = nextConfig;
