e799cecf72a0c377abdfc7684e07966bd93e7e42
Hiroki Chiyoda <contact@arafipro.com>
2024-01-04 09:12:15 +0900

    デプロイの設定を改める

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -2,6 +2,8 @@ import { connect, disconnect } from "@/utils/db";
 import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export const GET = async (req: Request, res: NextResponse) => {
   try {
     await connect();
